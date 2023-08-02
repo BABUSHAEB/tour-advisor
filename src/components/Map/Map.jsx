0ce22202557fb5617607f9ff76c9fdb9d7b9577f
babushaeb <abhisheksingj123@gmail.com>
2023-08-02 16:21:21 +0530

    card created in map

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -4,16 +4,12 @@ import GoogleMapReact from "google-map-react";
 import { Box, Paper, Rating, Typography, useMediaQuery } from "@mui/material";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
 import MapStyles from "./MapStyles";
-import { Marker } from "@react-google-maps/api";
 
 export const Map = ({ setCoordinates, setBounds, coordinates, places }) => {
   const isDesktop = useMediaQuery("(min-width:600px)");
   // alert(isDesktop);
-  console.log(places);
+  // console.log(places);
 
-  // const coordinates = { lat: 0, lng: 0 };
-
-  // console.log(places[1].longitude, places[1].latitude);
   return (
     <Box
       sx={{
@@ -22,7 +18,6 @@ export const Map = ({ setCoordinates, setBounds, coordinates, places }) => {
         // position: "relative",
       }}
     >
-      {/* <Box className={ClassNames.mapContainer}> */}
       <GoogleMapReact
         bootstrapURLKeys={{ key: process.env.REACT_APP_MAP_KEY }}
         defaultCenter={coordinates}
@@ -32,7 +27,7 @@ export const Map = ({ setCoordinates, setBounds, coordinates, places }) => {
         options={{
           // disableDefaultUI: true,
           zoomControl: true,
-          // styles: MapStyles,
+          styles: MapStyles,
         }}
         onChange={(e) => {
           setCoordinates({ lat: e.center.lat, lng: e.center.lng });
@@ -46,15 +41,15 @@ export const Map = ({ setCoordinates, setBounds, coordinates, places }) => {
               lat={Number(place.latitude)}
               lng={Number(place.longitude)}
               key={i}
-              // style={{
-              //   position: "absolute",
-              //   transform: "translate(-50%, -50%)",
-              //   zIndex: 1,
-              //   "&:hover": { zIndex: 2 },
-              // }}
+              style={{
+                position: "absolute",
+                transform: "translate(-50%, -50%)",
+                zIndex: 1,
+                "&:hover": { zIndex: 2 },
+              }}
             >
-              <LocationOnIcon color="secondary" fontSize="larger" />
-              {/* {!isDesktop ? (
+              {/* <LocationOnIcon color="secondary" fontSize="larger" /> */}
+              {!isDesktop ? (
                 <LocationOnIcon color="primary" fontSize="5rem" />
               ) : (
                 <Paper
@@ -79,7 +74,7 @@ export const Map = ({ setCoordinates, setBounds, coordinates, places }) => {
                       cursor: "pointer",
                     }}
                     src={
-                      place.photo
+                      place?.photo
                         ? place.photo.images.large.url
                         : "https://images.pexels.com/photos/67468/pexels-photo-67468.jpeg?auto=compress&cs=tinysrgb&w=600"
                     }
@@ -87,7 +82,7 @@ export const Map = ({ setCoordinates, setBounds, coordinates, places }) => {
                   />
                   <Rating size="small" value={Number(place.rating)} readOnly />
                 </Paper>
-              )} */}
+              )}
             </div>
           ))}
       </GoogleMapReact>
